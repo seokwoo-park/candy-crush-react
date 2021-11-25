2525908a95bfba47dd03bba4db5272476f34a955
seokwoo-park <p.seokwoo@gmail.com>
2021-11-25 22:43:36 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+
+const width = 8
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow'
+]
+
+
+function App() {
+
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for (let i = 0; i< width * width; i++) {
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+      randomColorArrangement.push(randomColor)
+    }
+    setCurrentColorArrangement(randomColorArrangement)
+  }
+
+  useEffect(()=> {
+    createBoard()
+  },[])
+
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App
